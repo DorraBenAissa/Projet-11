b4b5068a16e2c344a4904e1e72ffb8ee833b3818
DorraBenAissa <dorra.benaissaghariani@gmail.com>
2024-03-19 07:34:47 +0100

    Header

--- Frontend/src/layouts/Header/Header.jsx
+++ Frontend/src/layouts/Header/Header.jsx
@@ -1,10 +1,19 @@
 // IMPORTS
 import './header.scss'
-
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/img/argentBankLogo.png'
 
 function Header() {
    return (
-      <p>Header</p>
+      <header>
+         <nav className='cont-nav'>
+
+            <Link to="/">
+               <img className='logo-header' alt='Logo de Argent Bank' src={Logo} />
+            </Link>
+
+         </nav>
+      </header>
 
    )
 }
